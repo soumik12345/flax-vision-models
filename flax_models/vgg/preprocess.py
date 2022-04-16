11d6287870a5b6fa3bfae39d56188919b89fc43f
soumik12345 <19soumik.rakshit96@gmail.com>
2022-04-16 19:56:08 +0000

    added preprocess function for vgg module

--- flax_models/vgg/preprocess.py
+++ flax_models/vgg/preprocess.py
@@ -4,9 +4,9 @@ import jax.numpy as jnp
 from typing import List, Optional
 
 
-def preprocess(x, mean: Optional[List[float]] = None, std: Optional[float] = None):
+def preprocess(x, mean: Optional[List[float]] = None, std: Optional[float] = None, dtype=jnp.float64):
     """"Reference: https://github.com/keras-team/keras/blob/d8fcb9d4d4dad45080ecfdd575483653028f8eda/keras/applications/imagenet_utils.py#L168"""
-    x = x.astype(jnp.float64)
+    x = x.astype(dtype)
     x = x[..., ::-1]
     mean = [103.939, 116.779, 123.68] if mean is None else mean
     x[..., 0] -= mean[0]
--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-from flax_models.vgg import build_vgg16, build_vgg19
+import numpy as np
+
+from flax_models.vgg import build_vgg19
 
 
-model, parameters = build_vgg19(show_parameter_overview=True)
+model, parameters = build_vgg19(show_parameter_overview=True, include_top=False)
+x = np.random.normal(size=(1, 224, 224, 3))
+out = model.apply(parameters, x)
+print(out.shape)
