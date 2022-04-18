29900d69e46409c35c59304debc55a3dd501ea3f
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2022-04-18 05:31:30 +0000

    updated python packaging

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ import re
 import setuptools
 
 
+def read(*names, **kwargs):
+    with io.open(os.path.join(os.path.dirname(__file__), *names),
+                 encoding=kwargs.get('encoding', 'utf8')) as fp:
+        return fp.read()
+
+
 def find_version(*file_paths):
     version_file = read(*file_paths)
     version_match = re.search(r'^__version__ = ["\']([^"\']*)["\']', version_file, re.M)
