3d8f854f4789ae3cdee3fe7d0239874e86247a0c
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2022-04-18 05:35:51 +0000

    updated python packaging

--- setup.py
+++ setup.py
@@ -3,15 +3,10 @@ import re
 import setuptools
 
 
-def read(*names, **kwargs):
-    with io.open(os.path.join(os.path.dirname(__file__), *names),
-                 encoding=kwargs.get('encoding', 'utf8')) as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r'^__version__ = ["\']([^"\']*)["\']', version_file, re.M)
+def find_version(file_path):
+    with open(file_path, "r", encoding="utf-8") as fh:
+        file_content = fh.read()
+    version_match = re.search(r'^__version__ = ["\']([^"\']*)["\']', file_content, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError('Unable to find version string.')
@@ -26,7 +21,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="flax-vision-models",
-    version=find_version('flax_models', '__init__.py'),
+    version=find_version(os.path.join('flax_models', '__init__.py')),
     author="Soumik Rakshit",
     author_email="19soumik.rakshit96@gmail.com",
     description="A repository of Deep Learning models in Flax",
@@ -37,7 +32,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    # package_dir={"": "flax_models"},
     packages=setuptools.find_packages(exclude=("notebooks/")),
     python_requires=">=3.7.12,<3.11",
     install_requires=dependencies,
